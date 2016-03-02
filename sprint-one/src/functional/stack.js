98bc06c8aef257a4773c02fa010fd70171a64093
CodeRISHI <arunv83@gmail.com>
2016-03-02 14:49:45 -0800

    Complete all Stack prompts
    Lizzy/Arun

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,24 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var items = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[items] = value;
+    items++;
+
   };
 
   someInstance.pop = function() {
+    if (items > 0) {
+      items--;
+    }
+    return storage[items];
   };
 
   someInstance.size = function() {
+    return items;
   };
 
   return someInstance;
