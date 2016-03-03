41d26480d6c66da27b528a534648fff9f03f3150
CodeRISHI <arunv83@gmail.com>
2016-03-02 22:02:09 -0800

    Complete all specs for functional-shared queue prompts
    Lizzy/Arun

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.items = 0;
+
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  for (var i = this.items; i > 0; i--) {
+    this[i] = this[i - 1];
+  }
+
+  this[0] = value;
+  this.items++;
+};
+
+queueMethods.dequeue = function(value) {
+  if (this.items > 0) {
+    var temp = this[this.items - 1];
+
+    delete this[this.items - 1];
+    this.items--;
+    return temp;
+  }
+};
+
+queueMethods.size = function() {
+  return this.items;
+};
 
 
