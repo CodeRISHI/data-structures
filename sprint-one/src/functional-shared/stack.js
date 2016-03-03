954ed090296b3deae41c96ecd4eeaec39c64b99c
CodeRISHI <arunv83@gmail.com>
2016-03-02 21:41:49 -0800

    Complete all specs for functional-shared stack
    Arun/Lizzy

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
-var Stack = function() {
+var Stack = function(value) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStorage = {};
+  newStorage.value = value;
+  newStorage.items = 0;
+
+  // debugger;
+  _.extend(newStorage, stackMethods);
+  console.log(newStorage);
+  console.log(Object.keys(Stack));
+
+  return newStorage;
 };
 
-var stackMethods = {};
 
+stackMethods = {};
+
+stackMethods.push = function(value) {
+  this[this.items] = value;
+  this.items++;
+
+}; 
+
+stackMethods.pop = function() {
+  if (this.items > 0) {
+    this.items--;
+  }
+  return this[this.items];
+};
+
+stackMethods.size = function() {
+  return this.items;
+};
 
