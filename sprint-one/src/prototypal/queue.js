00b85ed45f5e27cb66bd3e8c5de15748a512de79
CodeRISHI <arunv83@gmail.com>
2016-03-03 10:04:05 -0800

    Complete Queue prompts for Prototypal Instantiation
    Arun/Lizzy

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,37 @@
-var Queue = function() {
+var Queue = function(value) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.items = 0;
+  newQueue.value = value;
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  for (var i = this.items; i > 0; i--) {
+    this[i] = this[i - 1];
+  }
+  this[0] = value;
+  this.items++;
+
+};
+
+queueMethods.dequeue = function() {
+  if (this.items > 0) {
+    var temp = this[this.items - 1];
+    delete this[this.items - 1];
+    this.items--;
+    
+    return temp;
+  }
+
+};
+
+queueMethods.size = function() {
+  return this.items;
+
+};
 
