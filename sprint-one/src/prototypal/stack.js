f474980db5748acef92b5277c24976dfae270bf1
CodeRISHI <arunv83@gmail.com>
2016-03-03 09:53:34 -0800

    Complete Stack prompts for Prototypal-Instantiation
    Arun/Lizzy

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
-var Stack = function() {
+var Stack = function(value) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.items = 0;
+  newStack.value = value;
+
+  return newStack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this[this.items] = value;
+  this.items++;
+};
+
+stackMethods.pop = function() {
+  if (this.items > 0) {
+    this.items--;
+  }
+
+  return this[this.items];
+
+};
+
+stackMethods.size = function() {
+  // debugger;
+  return this.items;
+};
 
 
