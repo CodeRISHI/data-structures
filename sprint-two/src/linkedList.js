7180dda66caa68d8a17943695edc4c7a8ab05553
CodeRISHI <arunv83@gmail.com>
2016-03-03 15:44:28 -0800

    Complete Linked List in Data Structures Sprint 2
    Lizzy/Arun

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,15 +2,47 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.nodes = 0;
 
   list.addToTail = function(value) {
+    if (!this.tail) {
+      this.tail = new Node(value);
+      this.head = this.tail;
+      this.nodes++;
+
+    } else {
+      this.tail.next = new Node(value);
+      this.tail = this.tail.next;
+    }
+
   };
 
   list.removeHead = function() {
+    var temp = this.head.value;
+    this.head = this.head.next;
+
+    return temp;
   };
 
   list.contains = function(target) {
+    //set a temp var to current head in list
+    var current = this.head;
+    var found = false;
+
+    var _contains = function(node) {
+      if (node.value === target) {
+        found = true;
+        return found;
+      } else if (node.next) {
+        _contains(node.next);
+      }
+    };
+
+    _contains(current);
+    return found;
+
   };
+
 
   return list;
 };
