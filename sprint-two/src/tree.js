0685345c296ae96c9a4caeeef1aaffaad2689306
CodeRISHI <arunv83@gmail.com>
2016-03-03 19:18:14 -0800

    Complete tree prompts
    Arun/Lizzy

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,7 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,13 +9,35 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newNode = new Tree(value);
+  this.children.push(newNode);
+
 };
 
 treeMethods.contains = function(target) {
+  var found = false;
+
+  var _contains = function(node) {
+    if (node.value === target) {
+      found = true;
+      return found;
+    } else {
+      if (node.children) {
+        for (var i = 0; i < node.children.length; i++) {
+          _contains(node.children[i]);
+        }
+        
+      }     
+    }
+  };
+  _contains(this);
+  return found;
+
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+//addChild - constant time complexity - O(1)
+//contains - linear time complexity - O(n)
+
  */
