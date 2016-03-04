b764ef7831c3b1b573039bc0ebdaefefa5c0bf12
CodeRISHI <arunv83@gmail.com>
2016-03-03 21:05:29 -0800

    Complete all but one prompts for binarySearchTree
    Arun/Lizzy

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,68 @@
 var BinarySearchTree = function(value) {
+  var newBST = Object.create(bstMethods);
+
+  newBST.value = value;
+  newBST.left = null;
+  newBST.right = null;
+
+  return newBST;
+};
+
+var bstMethods = {};
+
+bstMethods.insert = function(value) {
+  var newNode = new BinarySearchTree(value);
+  
+  var _insert = function(node) {
+    // debugger;
+    if (node.value > value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        _insert(node.left);
+      }
+    } else if (node.value < value) {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        _insert(node.right);
+      }
+    }
+  };
+  _insert(this);  
+};
+
+bstMethods.contains = function(target) {
+  var found = false;
+
+  var _contains = function(node) {
+    if (node.value === target) {
+      found = true;
+      return found;
+    } 
+    if (node.left !== null && node.left.value === target) {
+      found = true;
+      return found;
+    } else if (node.left !== null && node.left.value !== target) {
+      _contains(node.left);
+    } 
+
+    if (node.right !== null && node.right.value === target) {
+      found = true;
+      return found;
+    } else if (node.right !== null && node.right.value !== target) {
+      _contains(node.right);
+    }
+  };
+
+  _contains(this);
+  return found;
+};
+
+
+
+bstMethods.depthFirstLog = function() {
+
 };
 
 
