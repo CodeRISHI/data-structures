27ff0b60715383e8def9fa1fc6cb2ac40ce3f6e3
CodeRISHI <arunv83@gmail.com>
2016-03-04 21:40:14 -0800

    Complete Hash Table prompts - All specs pass
    Lizzy/Arun

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,16 +6,47 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  var tuple = [k, v]; 
+  var found = false;
+
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index] === undefined) {
+    this._storage[index] = [];
+  }
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      found = true;
+      this._storage[index][i][1] = v;
+    }
+  }
+  if (!found) {
+    this._storage[index].push(tuple);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      return this._storage[index][i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      return this._storage[index].splice(i, 1);
+    }
+  }
 };
+
+//Check if # of keys / array.length > (array.length * 0.75)
+  //Double size (this._limit) of array and rehash current tuples
+//if keys / array.length < (array.length * 0.25)
+  //Halve size (this._limit) of array and rehash current tuples
 
 
 
